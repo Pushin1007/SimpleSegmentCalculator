a80cb5acafc6459918f0731a78939fc55f9be7d4
Pushin <geiby@mail.ru>
2023-07-31 21:37:55 +0300

    add MainFragment and TextInputLayout

--- app/src/main/java/com/ws/simplesegmentcalculator/MainActivity.kt
+++ app/src/main/java/com/ws/simplesegmentcalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ws.simplesegmentcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.ws.simplesegmentcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance()).commit()
     }
 }
--- app/src/main/java/com/ws/simplesegmentcalculator/MainFragment.kt
+++ app/src/main/java/com/ws/simplesegmentcalculator/MainFragment.kt
@@ -1,4 +1,38 @@
 package com.ws.simplesegmentcalculator
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ws.simplesegmentcalculator.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    val binding: FragmentMainBinding
+        get() {
+            return _binding!!
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance(): MainFragment {
+            return MainFragment()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
