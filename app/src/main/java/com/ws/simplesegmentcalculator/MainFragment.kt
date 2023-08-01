17c831c1f86ce309b5679c2b259ea0de62b68c38
Pushin <geiby@mail.ru>
2023-08-02 00:56:30 +0300

    add Calculation

--- app/src/main/java/com/ws/simplesegmentcalculator/MainFragment.kt
+++ app/src/main/java/com/ws/simplesegmentcalculator/MainFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.ws.simplesegmentcalculator.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    private var aGrad: Double = 0.0
+    private var a: Double = 0.0
+    private var r: Double = 0.0
 
     private lateinit var parentActivity: MainActivity // Получаем родительскую активити
     override fun onAttach(context: Context) {
@@ -38,50 +42,47 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.appCompatImageView.setOnClickListener {
 
-            var outerDiameter =binding.inputOuterDiameterVal.text.toString().toDoubleOrNull()
-            var ringWidth =binding.inputRingWidthVal.text.toString().toDoubleOrNull()
-            var AllowancePerSide =binding.inputAllowancePerSideVal.text.toString().toDoubleOrNull()
-            var countSegment =binding.inputCountSegmentVal.text.toString().toDoubleOrNull()
+            var outerDiameter = binding.inputOuterDiameterVal.text.toString().toDoubleOrNull()
+            var ringWidth = binding.inputRingWidthVal.text.toString().toDoubleOrNull()
+            var AllowancePerSide = binding.inputAllowancePerSideVal.text.toString().toDoubleOrNull()
+            var countSegment = binding.inputCountSegmentVal.text.toString().toIntOrNull()
 
 
-            binding.calculateAngle.setText("$outerDiameter\u00B0")
+            if (countSegment != null) {
+                if (countSegment != 0) {
+                    aGrad = (360 / countSegment).toDouble()//угол сегмента в градусах
+                    a = aGrad * (Math.PI / 180) //угол сегмента в радианах
+                } else {
+                    Toast.makeText(parentActivity, "not 0", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(parentActivity, "enter", Toast.LENGTH_SHORT).show()
+            }
 
-            /*
-    public void calculateSegments(View view) {
+            if (outerDiameter != null) {
+                if (outerDiameter != 0.0) {
+                    r = outerDiameter / 2
+                } else {
+                    Toast.makeText(parentActivity, "not 0", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(parentActivity, "enter", Toast.LENGTH_SHORT).show()
+            }
 
-        EditText numSeg = findViewById(R.id.numSeg);
-        EditText diamEdit = findViewById(R.id.diamEdit);
-        EditText widthEdit = findViewById(R.id.widthEdit);
+            binding.calculateAngle.setText("$aGrad\u00B0")
 
-
-        Double a=0.0; //угол сегмента в радианах
-        Double aGrad=0.0; //угол сегмента в градусах
-        Double l=0.0; // длина сегмента
-        Double h=0.0; //высота сегмента
-        Double r= Double.parseDouble(String.valueOf(diamEdit.getText())) / 2;
-
-        aGrad = 360/Double.parseDouble(String.valueOf(numSeg.getText()));
-
-        a = aGrad * (Math.PI / 180);
-        l = 2 * Math.tan(a / 2) * r;
-        h = r - (r - Double.parseDouble(String.valueOf(widthEdit.getText()))) * Math.cos(a / 2);
-
-        TextView angleCalc = findViewById(R.id.angleCalc);
-        TextView lengthCalc = findViewById(R.id.lengthCalc);
-        TextView widthCalc = findViewById(R.id.widthCalc);
-
-        //str = new DecimalFormat("#.0#").format(d)
-
-        angleCalc.setText(new DecimalFormat("#.0#").format(aGrad));
-        lengthCalc.setText(new DecimalFormat("#.0#").format(l));
-        widthCalc.setText(new DecimalFormat("#.0#").format(h));
-
+            binding.calculateWidth.setText(String.format("%.1f", (2 * Math.tan(a / 2) * r)))
+            binding.calculateHeight.setText(
+                String.format(
+                    "%.1f",
+                    (r - (r - ringWidth!! * Math.cos(a / 2)))
+                )
+            )
 
 
-    }
-             */
         }
     }
+
     companion object {
         fun newInstance(): MainFragment = MainFragment()
     }
